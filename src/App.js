b92ae05500fedf8f606d39e4bd8e155e1a10064a
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-05 01:35:29 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+   <Box>
+    <Navbar />
+    <Routes>
+        <Route path='/' element= {<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />}/>
+    </Routes>
+   </Box>
   )
 }
 
