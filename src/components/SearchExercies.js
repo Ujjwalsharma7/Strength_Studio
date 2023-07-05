a986de258f63a8591585f2770902533c4b5de9bd
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-05 18:07:19 +0530

    Added typegrphy and textfield to searchexercises

--- src/components/SearchExercies.js
+++ src/components/SearchExercies.js
@@ -1,10 +1,20 @@
+import { useEffect, useState } from "react"
 import React from 'react'
+import { Box, Button, Stack, TextField, Typography } from "@mui/material"
 
 const SearchExercies = () => {
   return (
-    <div>
-      
-    </div>
+   <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
+    <Typography fontWeight={700} sx={{fontSize:{lg: '44px' , xs:'30px'}}} mb='50px' textAlign='center'>Awesome Exercises You <br /> Should Know</Typography>
+    <Box>
+      <TextField
+      height='76px'
+      value=''
+      onChange={(e) => {}}
+      placeholder="Search Exercies"
+      type="text" />
+    </Box>
+   </Stack>
   )
 }
 
