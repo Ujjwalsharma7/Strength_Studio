e74efe9281b7738e00234834772220d085397140
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-05 13:05:43 +0530

    added banner photo and styles

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import { Box, Stack, Typography, Button} from '@mui/material'
-const HeroBanner = () => {
-  return (
-    <Box sx={{
-      mt: {lg: '212px' , xs: '70px'},
-      ml: {sm: '50px'}
-    }}>
-        <Typography color='#ff2625' fontWeight='600px ' fontSize='26px'>Fitness Club</Typography>
-    <Typography fontWeight={700} sx={{fontSize: {lg: '44px', xs:'40px'}
-   }}>
-      SETS, REPS <br /> and GAINS 
-    </Typography>
-    <Typography fontSize='22px' lineHeight='35px' mb={3}>Check out the most effective exercises</Typography>
-    <Button variant="contained"  color='error'>Explore Exercises</Button>
-    </Box>
-  )
-}
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
-export default HeroBanner
+import HeroBannerImage from '../assets/images/banner.png';
+
+const HeroBanner = () => (
+  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" >
+    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
+    <Typography fontWeight={600} sx={{ fontSize: { lg: '40px', xs: '40px' } }} mb="2px" mt="10px">
+      SETS, REPS <br />
+      and GAINS
+    </Typography>
+    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+      Check out the most effective exercises for you
+    </Typography>
+    <Stack>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+    </Stack>
+    <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+      Exercise
+    </Typography>
+    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+  </Box>
+);
+
+export default HeroBanner;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Stack } from '@mui/material'
 
-import Logo from '../assets/assets/images/logo2.png'
+import Logo from '../assets/images/logo2.png'
 
 const Navbar = () => {
   return (
     <Stack direction='row' justifyContent='space-around' sx={{gap:{sm:'122px' , xs: '40px'} , mt: {sm: '32px' , xs: '20px'}, justifyContent:'none'}} px='20px'>
       <Link to="/">
-        <img src={Logo} alt='logo' style={{ width: '69px', height: '60px', margin: '0 10px'  }} />
+        <img src={Logo} alt='logo' style={{ width: '69px', height: '60px', margin: '-20px 10px'  }} />
       </Link>
       <Stack direction='row' gap='40px' fontSize='25px' alignItems='flex-end'>
         <Link to='/' style={
