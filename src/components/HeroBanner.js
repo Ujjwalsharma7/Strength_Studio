3fc853f0a0b357532517a14f852d69a9f900dc82
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-05 13:09:01 +0530

    Added minor changes in banner styles

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -5,7 +5,7 @@ import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" >
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
+    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Strength Studio </Typography>
     <Typography fontWeight={600} sx={{ fontSize: { lg: '40px', xs: '40px' } }} mb="2px" mt="10px">
       SETS, REPS <br />
       and GAINS
