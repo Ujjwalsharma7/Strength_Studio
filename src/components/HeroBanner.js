11e3d95de5304a042fb812cacb8be18cacfc6c2c
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-05 12:03:01 +0530

    added typography and button to herobanner compoent

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import { Box, Stack, Typography, Button} from '@mui/material'
 const HeroBanner = () => {
   return (
-    <div>
-      
-    </div>
+    <Box sx={{
+      mt: {lg: '212px' , xs: '70px'},
+      ml: {sm: '50px'}
+    }}>
+        <Typography color='#ff2625' fontWeight='600px ' fontSize='26px'>Fitness Club</Typography>
+    <Typography fontWeight={700} sx={{fontSize: {lg: '44px', xs:'40px'}
+   }}>
+      SETS, REPS <br /> and GAINS 
+    </Typography>
+    <Typography fontSize='22px' lineHeight='35px' mb={3}>Check out the most effective exercises</Typography>
+    <Button variant="contained"  color='error'>Explore Exercises</Button>
+    </Box>
   )
 }
 
