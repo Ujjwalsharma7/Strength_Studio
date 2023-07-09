50a9b69954a45a57b203a0a687797fbe7ea5b2fb
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-09 08:31:27 +0530

    added typography to Detail.js

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -11,6 +11,15 @@ const Detail = ({exerciseDetail}) => {
   return (
     <Stack gap='60px' sx={{flexDirection: {lg: 'row'}, p:'20px', alignItems:'center'}}>
         <img src={gifUrl} alt={name}  loading='lazy' className='detail-image' />
+        <Stack sx={{gap: {lg:'35px', xs:'20px'}}}>
+            <Typography variant='h3'>
+                {name}
+            </Typography>
+            <Typography>
+                Exercises keep in shape. {name} {` `} is one of the best
+                exercises to target your {target}. It will help you improve your mood and gain energy.
+            </Typography>
+        </Stack>
     </Stack>
   )
 }
