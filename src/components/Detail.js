e250e245a0e24a536bd3645d79747ffb06fed911
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-09 08:54:15 +0530

    added extra details to Detail.js

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -8,17 +8,42 @@ import EquipmentImage  from '../assets/icons/equipment.png';
 const Detail = ({exerciseDetail}) => {
  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
 
+ const extraDetail = [
+    {
+        icon: BodyPartImage,
+        name: bodyPart
+    },
+    {
+        icon: TargetImage,
+        name: target
+    },
+    {
+        icon: EquipmentImage,
+        name: equipment
+    }
+ ]
+
   return (
     <Stack gap='60px' sx={{flexDirection: {lg: 'row'}, p:'20px', alignItems:'center'}}>
         <img src={gifUrl} alt={name}  loading='lazy' className='detail-image' />
         <Stack sx={{gap: {lg:'35px', xs:'20px'}}}>
-            <Typography variant='h3'>
+            <Typography variant='h3' textTransform='capitalize'>
                 {name}
             </Typography>
-            <Typography>
+            <Typography textTransform='capitalize'>
                 Exercises keep in shape. {name} {` `} is one of the best
                 exercises to target your {target}. It will help you improve your mood and gain energy.
             </Typography>
+            {extraDetail.map((item) => (
+                <Stack key={item.name} direction='row' gap='24px' alignItems='center'>
+                    <Button >
+                        <img src={item.icon} />
+                        <Typography variant='h5'>
+                            {item.name}
+                        </Typography>
+                    </Button>
+                </Stack>
+           ) )}
         </Stack>
     </Stack>
   )
