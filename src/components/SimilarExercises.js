571b5e70f090cb004286983cc27b9b58bb462875
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-10 19:27:06 +0530

    added footer

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -14,7 +14,7 @@ const SimilarExercises = ({targetMuscleExercises, equipmentExercises}) => {
     {targetMuscleExercises.length ? <HorizontalScrollbar data={targetMuscleExercises} />  : <Loader />  }
     </Stack>
 
-    <Typography variant='h3' mb='1rem'>
+    <Typography variant='h3' mb='1rem' mt='30px'>
       Exerses that target use the same Equipment
     </Typography>
     <Stack direction='row' sx={{p:'2', position: 'relative'}}>
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
 
+import Logo from '../assets/images/logo-no-background.png'
 const footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='20px' alignItems='center' px='40px' pt='24px'>
+        <img src={Logo} alt='' width='200px' height='40px' />
+        <Typography variant='h5' pb='20px' >
+          Made with ❤️ by Ujjwalsharma7 
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
