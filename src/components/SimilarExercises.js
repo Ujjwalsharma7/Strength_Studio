0e63bb8454785f82fcbc549c38475783b20c7d24
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-10 18:08:27 +0530

    added typography to similar exercises

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Box, Typography, Stack } from '@mui/material';
 
-const SimilarExercises = () => {
+import HorizontalScrollbar from './HorizontalScrollbar';
+
+const SimilarExercises = ({targetMuscleExercises, equipmentExercises}) => {
   return (
-    <div>
-      
-    </div>
+   <Box sx={{mt: { lg:'100px', xs: '0'}}}>
+    <Typography variant='h4'>
+      Exerses that target the same muscle group
+    </Typography>
+  
+
+   </Box>
   )
 }
 
