f1259af6a6e7eae3ae3b03af52d2cefafce9890b
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-12 16:39:08 +0530

    added images and typography for BMI

--- src/components/BMIForm.js
+++ src/components/BMIForm.js
@@ -1,52 +1,56 @@
 import React from 'react'
-import { Box, TextField, Stack, Button} from '@mui/material'
+import { Box, TextField, Stack, Button, Typography } from '@mui/material'
 
 
 
 const BMIForm = () => {
 
-  return (
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '50ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-     <Stack direction='row'>
+    return (
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '50ch' },
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <Stack direction='row'>
 
-<Stack sx={{margin:'20px'}}>
+                <Stack sx={{ margin: '20px' }}>
 
-       
-        <TextField
-         type='number'
-         id="weight"
-         label="Weight"
-         defaultValue=""
-         helperText="(in Kgs)"
-         
-         color='error'
-         />
-         </Stack>
-     
-         <Stack sx={{margin:'20px'}}>
-      <TextField
-         type='number'
-         id="hieght"
-         label="Hieght"
-         defaultValue=""
-         helperText="(in cm)"
-         color='error'
-         />
-     </Stack>
 
-    
-         </Stack>
-   
-         <Button variant='contained' color='secondary' sx={{margin: '20px'}}>Calculate</Button>
+                    <TextField
+                        type='number'
+                        id="weight"
+                        label="Weight"
+                        defaultValue=""
+                        helperText="(in Kgs)"
+
+                        color='error'
+                    />
+                </Stack>
+
+                <Stack sx={{ margin: '20px' }}>
+                    <TextField
+                        type='number'
+                        id="hieght"
+                        label="Hieght"
+                        defaultValue=""
+                        helperText="(in cm)"
+                        color='error'
+                    />
+                </Stack>
+
+
+            </Stack>
+
+            <Button variant='contained' color='secondary' sx={{ margin: '20px' }}>Calculate</Button>
+
+            <Typography>Your BMI is: </Typography>
+            <Typography>Message</Typography> 
+            <Stack className='img-container'></Stack>
     </Box>
-  )
+    )
 }
 
 export default BMIForm
